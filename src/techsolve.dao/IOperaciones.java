a3a85e0a6d6bf9d2afd538df87c24f9de92e496e
Patrick <patrickcabrera2025@gmail.com>
2026-06-23 22:15:49 -0500

    Interfaz Operaciones añadida

--- src/techsolve.dao/IOperaciones.java
+++ src/techsolve.dao/IOperaciones.java
@@ -1,3 +1,12 @@
-public interface IOperaciones<T> {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
 
+//Interfaz de Operaciones DAO para las distintas clases.
+public interface IOperaciones<T> {
+    boolean insertar(T objeto);
+    List<T> listar();
+    Optional<T> buscarPorId(int id);
+    boolean actualizar(T objeto);
+    boolean eliminar(int id);
 }
